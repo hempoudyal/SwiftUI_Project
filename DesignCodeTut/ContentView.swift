55e1c29dc212d95e06139c6751fb85e0ee6b9f92
Hem Poudyal <hem.iosdev@gmail.com>
2020-04-15 10:48:17 +0545

    Initial Commit

---
+++ DesignCodeTut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeTut
+//
+//  Created by Hem Poudyal on 4/15/20.
+//  Copyright © 2020 Hem Poudyal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
